9c47848118bb651c95f5414a9f99057e655c6bcb
daan41114 <rendomgamer41114@gmail.com>
2020-12-02 23:33:24 +0100

    opdracht 1 toegevoegt

---
+++ development/opdracht_1.py
@@ -0,0 +1,18 @@
+import time
+from random import randint
+class me:
+
+
+    name='me'
+    hearts=3
+    lives=5
+    aantal_rupees= 10
+        
+    def _init_(self,lives):
+        self._lives=lives
+
+    def _init_(self,name):
+        self_name=name
+
+    print(lives)
+    print(name)
